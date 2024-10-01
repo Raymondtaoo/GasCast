b2a7bbf42f2fdf07a9976b648d9e89b931c73e5b
Raymond Tao <taor10@mcmaster.ca>
2024-10-01 17:08:31 -0400

    simulated ARIMA on crude oil

--- fetch/crude_oil_data.py
+++ fetch/crude_oil_data.py
@@ -5,10 +5,10 @@ def fetch_crude_oil_data():
     ticker = "CL=F"  # WTI Crude Oil Futures
 
     # Fetch historical data
-    crude_oil_data = yf.download(ticker, start="2022-01-01", end="2024-07-31")
+    crude_oil_data = yf.download(ticker, start="2000-01-01", end="2024-07-31")
 
     # Save data to CSV
-    crude_oil_data.to_csv('data/crude_oil_prices_2022_2024.csv')
+    crude_oil_data.to_csv('data/crude_oil_prices_2000_2024.csv')
 
     return crude_oil_data
 
